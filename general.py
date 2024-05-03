8bbbbdefd81f7c3a1d55dbd4359afa6e28ad6c75
antiantilopa <santakamenb@gmail.com>
2024-05-03 21:58:41 +0600

    debug, extend and comment general.py
    was fixed typiztion error, was added new methods and new class countable. everything had been commented

--- general.py
+++ general.py
@@ -1,6 +1,5 @@
-#from pygame import Surface
-class Surface:
-    pass
+from typing import Any
+from pygame import Surface
 
 class Alive:
     def __init__(self, value,  maxValue):
@@ -10,18 +9,23 @@ class Alive:
         self.value = min(self.value + value, self.maxValue)
     def gainDamage(self, value: int) -> bool:
         self.value -= value
-        return isAlive()
+        return self.isAlive()
     def isAlive(self) -> bool:
         return self.value > 0
+    def __call__(self) -> int:
+        return self.value
         
+
 class Texture:
-    def __init__(self, texture: Surface, size: (int, int), frames: int = 1 ):
+    def __init__(self, texture: Surface, size: tuple[int, int], frames: int = 1 ):
         self.texture: Surface = texture
         self.frames: int = frames
         self.current: int = 0
         self.width, self.height = size
     def nextFrame(self) -> None:
         self.current = (self.current + 1) % self.frames
-    def getFrameCoords(self, curr: int = None) -> (int, int, int, int):
+    def getFrameCoords(self, curr: int = None) -> tuple[int, int, int, int]:
         if curr == None: curr = self.current 
         return (self.size[0] * self.current, 0, self.size[0], self.size[1])
+    def blit(self, display: Surface, cords: tuple[int, int]) -> None:
+        self.texture.blit(display, cords, self.getFrameCoords)
