cc4a822a5c2132feedeb01a37dab2125b146917e
TAFH <talim2007@mail.ru>
2024-05-08 01:05:44 +0500

    BIG Refactoring of widgets library

--- source/menu/widgets.py
+++ source/menu/widgets.py
@@ -1,12 +1,51 @@
 import pygame
 
 
-class Widget:
+class Clicked:
+    def __init__(self, color: tuple[int, int, int], border: "Border"=None):
+        self.color = color
+        self.border = border
+    
+    @staticmethod
+    def get_default():
+        return Clicked((0, 0, 0))
+
+class Border:
+    
+    def __init__(self, width: int, color: tuple[int, int, int], radius: int):
+        self.width = width
+        self.color = color
+        self.radius = radius
+    
+    @staticmethod
+    def get_default():
+        return Border(1, (0, 0, 0), -1)
+
+class Text:
+
+    def __init__(self, text: str, 
+                 font: pygame.font.Font = None, 
+                 color: tuple[int, int, int] = (0, 0, 0),
+                 background_color: tuple[int, int, int] = None):
+        font = font or pygame.font.SysFont("Arial", 20)
+        self.text = text
+        self.font = font
+        self.color = color
+        self.background_color = background_color
+    
+    def render(self):
+        return self.font.render(self.text, False, self.color, self.background_color)
+    
+    @staticmethod
+    def get_default():
+        return Text('')
+    
+class RawWidget:
     """
-    Base class for all widgets.
+    Abstract widget class.
     """    
 
-    def __init__(self, x, y, width, height) -> None:
+    def __init__(self, x: int, y: int, width: int=100, height: int=100) -> None:
         self.x = x
         self.y = y
         self.width = width
@@ -17,82 +56,43 @@ class Widget:
         for i in self.childs:
             i.draw(window)
 
-    def update(self, events):
+    def update(self, events: list[pygame.event.Event]):
         for i in self.childs:
             i.update(events)
-
-
-class Text:
-
-    def __init__(self, text: str, 
-                 font: pygame.font.Font = None, 
-                 color: tuple[int, int, int] = (0, 0, 0)):
-        font = font or pygame.font.SysFont("Arial", 20)
-        self.text = text
-        self.font = font
-        self.color = color
-
-class Clicked:
-
-    def __init__(self, color, border):
-        self.color = color
-        self.border = border
-
-class Border:
+            
+class Widget(RawWidget):
     
-    def __init__(self, width, color, radius):
-        self.width = width
-        self.color = color
-        self.radius = radius
-
-class Label(Widget):
-
-    def __init__(self, x, y, text: Text):
-        self.text = text
-        width, height = self.text.font.render(self.text.text, False, self.text.color).get_size()
-
-        super().__init__(x, y, width, height)
-
-    def draw(self, window: pygame.Surface):
-        super().draw(window)
-
-        txt = self.text.font.render(self.text.text, False, self.text.color)
-        sz = txt.get_size()
-        window.blit(txt, (self.x, self.y) + sz)
-
-    def update(self, events):
-        super().update(events)
-
-
-class Button(Widget):
-    
-    def __init__(self, x, y, width, height, clicked: Clicked, color=(255, 0, 0), border: Border=None, cmd=lambda: (), 
-                 text=None, data=None):
-        Widget.__init__(self, x, y, width, height)
-        text = text or Text("")
-        
+    def __init__(self, x: int, y: int, width: int, height: int, 
+                 clicked: Clicked=None, color=None, border: Border=None,
+                 text: Text=None):
+        super().__init__(x, y, width, height)        
         self.text = text
         self.border = border
         self.color = color
-        self.cmd = cmd
         self.clicked = clicked
         self.is_clicked = False
-        self.data = data
         
-    def draw(self, window):
+    def draw(self, window: pygame.Surface):
         super().draw(window)
-
-        color = (self.color, self.clicked.color)[self.is_clicked]
-        border = (self.border, self.clicked.border)[self.is_clicked]
+        
+        color = self.color
+        border = self.border
+        if self.clicked and self.is_clicked:
+            color = self.clicked.color
+            border = self.clicked.border
+        
+    
         if border:
-            pygame.draw.rect(window, color, (self.x, self.y, self.width, self.height), 0, border.radius)
+            if color: 
+                pygame.draw.rect(window, color, (self.x, self.y, self.width, self.height), 0, border.radius)
             bw = border.width
             pygame.draw.rect(window, border.color, (self.x - bw, self.y - bw, self.width + bw, self.height + bw), bw, border.radius)
-        else:
+        elif color:
             pygame.draw.rect(window, color, (self.x, self.y, self.width, self.height))
         
-        txt = self.text.font.render(self.text.text, False, self.text.color)
-        window.blit(txt, (self.x + (self.width - txt.get_width()) / 2, self.y + (self.height - txt.get_height()) / 2) + txt.get_size())
+        if self.text:
+            txt = self.text.font.render(self.text.text, False, self.text.color)
+            window.blit(txt, (self.x + (self.width - txt.get_width()) / 2, self.y + (self.height - txt.get_height()) / 2) + txt.get_size())
 
     def update(self, events: list[pygame.event.Event]):
         super().update(events)
@@ -103,14 +103,48 @@ class Button(Widget):
                 self.is_clicked = True
             if i.type == pygame.MOUSEBUTTONUP and self.is_clicked:
                 self.is_clicked = False
-                self.cmd(self.data)
+                self.onclick()
+                
+    def onclick(self):
+        pass
+
+class Label(Widget):
+    """
+    Shorthand name of widget that is used only as text.
+    """
+    
+    def __init__(self, x, y, text: Text=None):
+        self.text = text or Text.get_default()
+        width, height = self.text.render().get_size()
+
+        super().__init__(x, y, width, height, text=text)
+
+    def draw(self, window: pygame.Surface):
+        super().draw(window)
+
+    def update(self, events):
+        super().update(events)
+
+
+class Button(Widget):
+    
+    def __init__(self, x: int, y: int, width: int, height: int, 
+                 clicked: Clicked=None, color=(255, 0, 0), border: Border=None, cmd=lambda: (), 
+                 text: Text=None, data=None):
+        
+        Widget.__init__(self, x, y, width, height, clicked, color, border, text)
+        self.cmd = cmd
+        self.data = data
+
+    def onclick(self):
+        self.cmd(self.data)
                 
 class Menu:
     is_disabled: bool
     widgets: list[Widget]
     name: str
     
-    def __init__(self, name) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
         self.is_disabled = False
         self.widgets = []
@@ -118,7 +152,7 @@ class Menu:
     def add_widget(self, widget: Widget):
         self.widgets.append(widget)
         
-    def draw(self, window):
+    def draw(self, window: pygame.Surface):
         if self.is_disabled: return
         for i in self.widgets:
             i.draw(window)
--- source/warandlie.py
+++ source/warandlie.py
@@ -1,7 +1,6 @@
 import pygame
 import logging
-from .game import game_cycle, init
-from .exceptions import Warning
+from .game import game_cycle, init, main_menu
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -18,9 +17,10 @@ def main():
         pygame.font.init()
         init()
         logging.info("Game initialized.")
+        main_menu()
         game_cycle()
     except Exception as e:
-        logging.critical(e)
+        logging.critical(e.with_traceback(None))
         exit(1)
     
     
